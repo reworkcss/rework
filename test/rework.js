4716e99399925488c9a1b862ff826e6293c797cd
TJ Holowaychuk <tj@vision-media.ca>
2012-07-13 15:58:42 -0700

    add test case

--- test/rework.js
+++ test/rework.js
@@ -16,11 +16,21 @@ describe('rework(css)', function(){
         .should.equal(fixture('prefix.out'));
     })
 
-    it('should prefix @keyframes', function(){
-      var str = rework(fixture('keyframes'))
-        .prefix('@keyframes', ['-webkit-', '-moz-'])
-        .toString()
-        .should.equal(fixture('keyframes.out'));
+    describe('when "@keyframes" is given', function(){
+      it('should prefix @keyframes', function(){
+        var str = rework(fixture('keyframes'))
+          .prefix('@keyframes', ['-webkit-', '-moz-'])
+          .toString()
+          .should.equal(fixture('keyframes.out'));
+      })
+
+      it('should prefix only using the parent vendor prefix', function(){
+        var str = rework(fixture('keyframes.props'))
+          .prefix('border-radius', ['-webkit-', '-moz-'])
+          .prefix('@keyframes', ['-webkit-', '-moz-'])
+          .toString()
+          .should.equal(fixture('keyframes.props.out'));
+      })
     })
   })
 })
