d86c6a73398397eb8092d40673e220b03d0afe63
TJ Holowaychuk <tj@vision-media.ca>
2012-07-13 15:28:17 -0700

    Initial commit

---
+++ test/rework.js
@@ -0,0 +1,19 @@
+
+var rework = require('..')
+  , fs = require('fs')
+  , read = fs.readFileSync;
+
+function fixture(name) {
+  return read('test/fixtures/' + name + '.css', 'utf8');
+}
+
+describe('rework(css)', function(){
+  describe('.prefix(prop, prefixes)', function(){
+    it('should prefix properties', function(){
+      rework(fixture('prefix'))
+        .prefix('border-radius', ['-webkit-', '-moz-'])
+        .toString()
+        .should.equal(fixture('prefix.out'));
+    })
+  })
+})
