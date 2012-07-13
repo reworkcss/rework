9da55b0f87e83a9248f9f4bd7ad9306519910bf1
TJ Holowaychuk <tj@vision-media.ca>
2012-07-13 16:21:43 -0700

    add more to the example

--- examples/keyframes.js
+++ examples/keyframes.js
@@ -7,9 +7,11 @@ var rework = require('..')
   , fs = require('fs')
   , read = fs.readFileSync;
 
+var vendors = ['-webkit-', '-moz-', '-ms-'];
+
 var css = rework(read('examples/keyframes.css', 'utf8'))
-  .prefix('border-radius', ['-webkit-', '-moz-'])
-  .prefix('@keyframes', ['-webkit-', '-moz-'])
+  .prefix('border-radius', vendors)
+  .prefix('@keyframes', vendors)
   .toString();
 
 console.log(css);
