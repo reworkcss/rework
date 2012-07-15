4fb22f93409b89a8a0a500172db38767b3e60066
TJ Holowaychuk <tj@vision-media.ca>
2012-07-15 16:55:53 -0700

    fix keyframe support by creating a new stylesheet node

--- examples/sink.js
+++ examples/sink.js
@@ -7,7 +7,7 @@ var rework = require('..')
   , fs = require('fs')
   , read = fs.readFileSync;
 
-var vendors = ['-webkit-', '-moz-'];
+var vendors = ['-webkit-', '-moz-', '-ms-'];
 
 var css = rework(read('examples/sink.css', 'utf8'))
   .prefix('border-radius', vendors)
