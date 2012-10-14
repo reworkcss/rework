4ba9e2e832f4d8f7af2634e26f7c2baadefca3b8
Andrei Oprea <andrei.br92@Gmail.com>
2012-10-14 22:29:15 +0300

    added support for gradients

--- examples/prefix.js
+++ examples/prefix.js
@@ -2,10 +2,12 @@
 var rework = require('..')
   , read = require('fs').readFileSync;
 
-var css = rework(read('examples/prefix.css', 'utf8'))
+var css = rework(read('./prefix.css', 'utf8'))
   .vendors(['-webkit-', '-moz-'])
   .use(rework.prefix('border-radius'))
   .use(rework.prefix('box-shadow'))
+  .use(rework.prefix('linear-gradient'))
+  .use(rework.prefix('radial-gradient'))
   .toString()
 
 console.log(css);
