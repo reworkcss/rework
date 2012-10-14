bb0e5fb55dcb183c791b8368787cde4519a7aaca
Andrei Oprea <andrei.br92@Gmail.com>
2012-10-14 22:31:37 +0300

    fixed example

--- examples/prefix.js
+++ examples/prefix.js
@@ -2,7 +2,7 @@
 var rework = require('..')
   , read = require('fs').readFileSync;
 
-var css = rework(read('./prefix.css', 'utf8'))
+var css = rework(read('examples/prefix.css', 'utf8'))
   .vendors(['-webkit-', '-moz-'])
   .use(rework.prefix('border-radius'))
   .use(rework.prefix('box-shadow'))
