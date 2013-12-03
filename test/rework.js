d88dc8f28606bd1a0ad8e6a746b88b1eeeb75c5f
Andrey Popp <8mayday@gmail.com>
2013-12-03 19:41:53 +0400

    Inline source maps as comments
    * rework(src, options) now accepts options arguments which is passed to
      css-parse, we set options.position = true so we can generate sourcemaps later
    
    * Rework.prototype.toString(options) accepts sourcemap and sourcemapAsObject
      boolean options. The first one instructs css-stringify to generate sourcemap
      and then inlines it in source as base64 encoded data URI. If sourcemapAsObject
      is true then result from css-stringify returned unmodified and thus no
      sourcemap is inlined.

--- test/rework.js
+++ test/rework.js
@@ -322,4 +322,27 @@ describe('rework', function(){
         .should.equal('body{color:red;}');
     })
   })
+
+  describe('.toString() sourcemap option', function() {
+    it('should inline sourcemap', function() {
+      rework('body { color: red; }')
+        .toString({ compress: true, sourcemap: true })
+        .should.equal(
+          'body{color:red;}' + '\n' +
+          '/*# sourceMappingURL=data:application/json;base64,' +
+               'eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdGVkLmNzc' +
+               'yIsInNvdXJjZXMiOlsic291cmNlLmNzcyJdLCJuYW1lcy' +
+               'I6W10sIm1hcHBpbmdzIjoiQUFBQSxLQUFPLFNBQVUifQ== */');
+    })
+  })
+
+  describe('.toString() sourcemapAsObject and sourcemap options', function() {
+    it('should return sourcemap as an object', function() {
+      var result = rework('body { color: red; }')
+        .toString({ compress: true, sourcemap: true, sourcemapAsObject: true });
+      result.code.should.equal('body{color:red;}');
+      result.map.should.have.property('mappings');
+      result.map.mappings.should.equal('AAAA,KAAO,SAAU');
+    })
+  })
 })
