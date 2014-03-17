7965b4c98c7d3d3d090888ea6c7ecd01659cb5a4
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-03-16 23:28:35 -0400

    Remove examples since they use non-existent bundled plugins

--- examples/colors.js
+++
@@ -1,9 +0,0 @@
-
-var rework = require('..')
-  , read = require('fs').readFileSync;
-
-var css = rework(read('examples/colors.css', 'utf8'))
-  .use(rework.colors())
-  .toString()
-
-console.log(css);
--- examples/extend-nesting.js
+++
@@ -1,9 +0,0 @@
-
-var rework = require('..')
-  , read = require('fs').readFileSync;
-
-var css = rework(read('examples/extend-nesting.css', 'utf8'))
-  .use(rework.extend())
-  .toString();
-
-console.log(css);
